31060b8be7aa52369c9b74d5efa3820fa39a74bd
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2020-02-20 14:21:23 +0400

    States #6, actions

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+const potato = 'potato';
+
+export default potato;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 /** @jsx h */
 import { app } from 'hyperapp'
-import { getStateFromStorage } from './utils/local-storage'
+import { getStateFromStorage } from './states/local-storage'
+import { Actions } from './actions/'
 import { View } from './views/'
 import 'bulma'
 
-const getInitialState = () => getStateFromStorage() || ({ items: [], value: '', placeholder: 'Add new todo' })
+const getInitialState = () => getStateFromStorage() || ({
+  containerVolume: 1000, 
+  hotWaterVolume: 250,
+  coldWaterVolume: 625,
+  starterMass: 125,
+  teaMass: 5,
+  sugarMass: 5,
+})
 
 app({
   init: getInitialState(),
