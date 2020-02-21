5b2734002a62f893d33c11581df76526ecd1a5ab
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2020-02-21 18:43:56 +0400

    views/footer

---
+++ src/views/footer.js
@@ -0,0 +1,9 @@
+import { h } from 'hyperapp';
+
+export const Footer = () => (
+  <footer class="hero-foot footer">
+    <div class="has-text-centered">
+      made with <span class="red">❤</span> by <a class="red" href="https://hayk.earth">Հայկ</a>
+    </div>
+  </footer>
+)
