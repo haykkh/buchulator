c6405727858e10ecf36598c1d89178ceaee1f973
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2020-02-24 16:20:29 +0400

    build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -24,40 +24,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "android-chrome-192x192.9defcb26.png",
-    "revision": "5a5b925d09d1f0a5ab55c273b43e9c0e"
-  },
-  {
-    "url": "android-chrome-512x512.cc191728.png",
-    "revision": "1c0d5f82c33566b0626f17ca4503d9d4"
-  },
-  {
-    "url": "apple-touch-icon.9fa4e4f6.png",
-    "revision": "fafc02c07fbd4c79c036863d207f0413"
-  },
-  {
-    "url": "favicon-16x16.71b6712a.png",
-    "revision": "59893e685ccea2c0a43ab3c175726dbf"
-  },
-  {
-    "url": "favicon-32x32.9f57072f.png",
-    "revision": "1bea9ca21fe49e7ff561fb54ea4549d3"
-  },
-  {
     "url": "index.html",
-    "revision": "01f019ee3017d3e9a4fb3f1743538232"
+    "revision": "6b72d783c3b931967ca1f5772139d2fa"
+  },
+  {
+    "url": "src.77386816.js",
+    "revision": "aa369fdcd5fca862d53695f6f0ad4a81"
   },
   {
     "url": "src.9c90f017.css",
     "revision": "ba276ee89680eb3bfa222a68c4795804"
   },
   {
-    "url": "src.fc89adcc.js",
-    "revision": "4dee89951b3a057bf265d18dfd52d160"
-  },
-  {
     "url": "/",
-    "revision": "3e2244260cd6e12471273789dddfb807"
+    "revision": "eba72368e62b570a5aaee6ea26ddf4a3"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
