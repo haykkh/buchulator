9374401177804f79acf92bc92f3c318bdc7c56e1
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2020-02-24 15:13:54 +0400

    views/units
    *  load unit button based on stored unit state
    *  for some reason i need to do metric.metric here to access bool value

--- src/views/units.js
+++ src/views/units.js
@@ -4,13 +4,13 @@ import { toImperial, toMetric } from '../actions'
 export const Units = metric => (
   <div class="buttons has-addons is-centered">
     <button 
-    class={metric ? "button is-warning is-light is-selected" : "button is-light"}
+    class={metric.metric ? "button is-warning is-light is-selected" : "button is-light"}
     id="metric" 
     onclick={toMetric}>
       Metric
     </button>
     <button 
-    class={metric ? "button is-light" : "button is-warning is-light is-selected"}
+    class={metric.metric ? "button is-light" : "button is-warning is-light is-selected"}
     id="imperial" 
     onclick={toImperial}>
       Imperial
