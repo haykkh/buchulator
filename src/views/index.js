f17581c354be3abccd67cb79e0078c430b984d1e
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2020-02-24 16:08:29 +0400

    No more state display

--- src/views/index.js
+++ src/views/index.js
@@ -1,12 +1,7 @@
-/* eslint-disable no-unused-vars */
 import { h } from 'hyperapp';
 import { Header } from './header';
 import { Calculator } from './calculator';
 import { Footer } from './footer';
-import { TodoList } from './todo-list';
-import { TodoInput } from './todo-input';
-import { TodoListDone } from './todo-list-done';
-import { StateDisplay } from './state-display';
 
 const View = (state) => (
   <section class='hero is-fullheight'>
@@ -14,7 +9,6 @@ const View = (state) => (
     <Calculator state={state} />
     <TodoList items={state.items} />
     <TodoListDone items={state.items} />
-    <StateDisplay state={state} />
     <Footer />
   </section>
 );
