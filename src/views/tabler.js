1b921186bff7cab9fdd0760d58b8cd5db50e7c90
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2020-02-24 15:16:42 +0400

    views/tabler
    Passes metric to measurement-input rather than unit

--- src/views/tabler.js
+++ src/views/tabler.js
@@ -1,13 +1,13 @@
 import { h } from 'hyperapp'
 import { MeasurementInput } from './measurement-input'
 
-const Rower = (stateVar, varText, ratio, unit, unitType) => (
+const Rower = (stateVar, varText, ratio, unitType, metric) => (
   <tr>
     <td class="vertical-middle">{varText}</td>
     <MeasurementInput 
     value={stateVar}
     ratio={ratio}
-    unit={unit}
+    metric={metric}
     unitType={unitType}
     />
   </tr>
@@ -15,11 +15,11 @@ const Rower = (stateVar, varText, ratio, unit, unitType) => (
 
 export const Tabler = ({ state }) => (
   <table class="table">
-    {Rower(state.containerVolume, 'Container Volume', 1, 'ml', 'volume')}
-    {Rower(state.hotWaterVolume, 'Hot Water Volume', 0.25, 'ml', 'volume')}
-    {Rower(state.coldWaterVolume, 'Cold Water Volume', 0.625, 'ml', 'volume')}
-    {Rower(state.starterMass, 'Starter Volume', 0.125, 'ml', 'volume')}
-    {Rower(state.teaMass, 'Tea Mass', 0.005, 'g', 'mass')}
-    {Rower(state.sugarMass, 'Sugar Mass', 0.05, 'g', 'mass')}
+    {Rower(state.containerVolume, 'Container Volume', 1, 'volume', state.metric)}
+    {Rower(state.hotWaterVolume, 'Hot Water Volume', 0.25, 'volume', state.metric)}
+    {Rower(state.coldWaterVolume, 'Cold Water Volume', 0.625, 'volume', state.metric)}
+    {Rower(state.starterMass, 'Starter Volume', 0.125, 'volume', state.metric)}
+    {Rower(state.teaMass, 'Tea Mass', 0.005, 'mass', state.metric)}
+    {Rower(state.sugarMass, 'Sugar Mass', 0.05, 'mass', state.metric)}
   </table>
 )
